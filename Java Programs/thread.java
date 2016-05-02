5109bd46fa5bc84e8e5008c1ed1e9e2c517cc2b2
t2013anurag <tiwari.anurag126@gmail.com>
2016-05-02 23:31:44 +0530

    Understanding threads

---
+++ Java Programs/thread.java
@@ -0,0 +1,42 @@
+class thread
+	{
+	public static void main(String args[])
+	{
+	new thtest();
+	try
+	{
+	for (int i = 1; i<= 5; i++)
+		{
+		System.out.println(i + " x  5 = " + i *5);
+		Thread.sleep(1000);
+		}
+	}
+	catch (Exception e){}
+	System.out.println("Main thread exit");
+	}
+	}
+
+class thtest extends Thread
+	{
+	
+	thtest() 
+		{ 	
+		start();
+		}
+
+	public void run()
+		{
+		try
+		{
+		for (int i = 1; i<= 5; i++)
+		{
+		System.out.println(i + " +  5 = " + i +5);
+		Thread.sleep(100);
+		}
+		}
+		catch(InterruptedException e){ System.out.println(e);}
+		
+		System.out.println("Child thread exit");
+		}
+	}
+					
